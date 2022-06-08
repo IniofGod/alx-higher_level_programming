1d47aaf481ea6021414b257a2110046c41265b9c
Herrygirl <blessingilesanmi22@gmail.com>
2022-06-08 13:44:39 -0700

    Update 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
-def roman_to_int(roman_string):
-    roman_numbers = [1, 5, 10, 50, 100, 500, 1000]
-    roman_letter = ['I', 'V', 'X', 'L', 'C', 'D', 'M']
-    if roman_string is None or type(roman_string) is not str:
-        return 0
-    integer = 0
-    lenght = len(roman_string)
-    for i, number in enumerate(roman_string):
-        for x, letter in enumerate(roman_letter):
-            if number == letter:
-                if lenght == i + 1:
-                    integer = integer + roman_numbers[x]
-                    break
-                elif letter == 'D':
-                        if roman_string[i + 1] == roman_letter[x + 1]:
-                            integer = integer - roman_numbers[x]
 
+
+# roman_to_int - converts a roman numeral to an integer.
+def roman_to_int(roman_string):
+    romans = {'I': 1, 'V': 5, 'X': 10, 'L':50, 'C': 100, 'D': 500, 'M': 1000,\
+	    'IV': 4, 'IX': 9, 'XL': 40, 'XC': 90, 'CD': 400, 'CM': 900}
+
+    if roman_string and type(roman_string) == str:
+        num = 0
+        for i in range(len(roman_string)):
+            for j in romans.keys():
+                if roman_string[:] is j:
+                    num = romans[j]
+                    return num
+                if roman_string[i:i+1] is j:
+                    num += romans[j]
+                    break
+        return num
+    else:
+        return 0
