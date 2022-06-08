7f83941a35aeded8e61bc60218de148f4f1a6289
Herrygirl <blessingilesanmi22@gmail.com>
2022-06-08 13:39:50 -0700

    Update 7-update_dictionary.py

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-def update_dictionary(a_dictionary, key, value):
-    presence = 0
-    for keys in a_dictionary:
-        if keys == key:
 
+
+# update_dictionary - replaces or adds key/valu in a dictionary.
+def update_dictionary(a_dictionary, key, value):
+    a_dictionary[key] = value
+    return a_dictionary
