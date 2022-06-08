bb04556ef36b7b1be23ea8c3e7683a9c06a09f6b
Herrygirl <blessingilesanmi22@gmail.com>
2022-06-08 13:53:14 -0700

    Update 100-weight_average.py

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 
-# weight_average - returns the weighted average of all intergers tuple.
 def weight_average(my_list=[]):
-
+    if len(my_list) == 0:
+        return 0
+    scores = [score*weight for (score, weight) in my_list]
+    return sum(scores) / sum([weight for (score, weight) in my_list])
